754c4d22221e16839e6328154edfdc2437982a1f
Jonathan Bogasky <boganaut@gmail.com>
2021-05-18 17:50:52 -0500

    Implemented ItemList class with AddItem, RemoveItem, and PrintOut methods. Tested basic input as well as invalid values.

--- Kata_Checkout/Program.cs
+++ Kata_Checkout/Program.cs
@@ -4,9 +4,40 @@ namespace Kata_Checkout
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] temp = new string[10];
+            ItemList list1 = new ItemList();
+            
+
+            while (true)
+            {
+                temp = Console.ReadLine().Split(' ');
+
+                if (temp[0] == "Q")
+                    System.Environment.Exit(1);
+                else if (temp[0] == "P")
+                {
+                    list1.PrintOut();
+                }
+                else
+                {
+                    try
+                    {
+                        list1.AddItem(temp[0], Convert.ToDecimal(temp[1]));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+
         }
     }
 }
